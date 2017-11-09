a7ff653ac4920b767ba393a3584427dc4b55de1b
Corwyn Nielsen <corwynnielsen@Corwyns-MacBook-Pro.local>
2017-11-09 13:29:02 -0500

    Finished

--- hw7/Solution.py
+++ hw7/Solution.py
@@ -1,5 +1,5 @@
 from sys import maxsize
-from queue import PriorityQueue
+import heapq
 class Solution:
     def __init__(self, start_node, end_node, graph):
         self.graph = graph
@@ -9,38 +9,33 @@ class Solution:
     def outputPath(self):
         distances = {self.start_node: 0}
         previous = {}
-        q = PriorityQueue()
+        q = []
         for i in range(len(self.graph)):
             if i != self.start_node:
                 distances[i] = maxsize
             previous[i] = None
-            q.put((distances[i], i))
-
-        while not q.empty():
-            vertex = (q.get())[1]
-            if vertex == self.end_node:
+            
+        heapq.heappush(q, (0, self.start_node))
+        while q:
+            weight, v = heapq.heappop(q)
+            if v == self.end_node:
                 break
 
-            for i in self.graph[vertex][1:]:
-                path = distances[vertex] + self.length(vertex, i)
+            for i in self.graph[v][1:]:
+                path = distances[v] + self.graph[i][0]
                 if path < distances[i]:
                     distances[i] = path
-                    previous[i] = vertex
+                    previous[i] = v
+                    heapq.heappush(q, (distances[i], i))
 
         return self.shortest_path(previous)
-
-    def length(self, node1, node2):
-        node1_weight = self.graph[node1][0]
-        node2_weight = self.graph[node2][0]
-        return abs(node1_weight-node2_weight)
 
     def shortest_path(self, previous):
          stack = []
          u = self.end_node
-         while u in previous.keys() and previous[u] != None:
+         while previous[u] != None:
              stack.append(u)
              u = previous[u]
-             print(u)
          stack.append(u)
          if len(stack) == 1:
              return []
